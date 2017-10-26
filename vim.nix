fe7dbde69d3fc6f401fb4c340d2f535c1f587148
chessai <chessai1996@gmail.com>
2017-10-26 16:38:12 -0400

    first commit

---
+++ vim.nix
@@ -0,0 +1,42 @@
+{config,pkgs,...}:
+
+let
+  myVim = pkgs.vim_configurable.customize {
+    name = "vim";
+    vimrcConfig.customRC = builtins.readFile ./vimrc;
+
+    vimrcConfig.packages.myVimPackage = with pkgs.vimPlugins; {
+      start = [
+        vim-colorschemes
+        rainbow_parentheses
+        syntastic
+        vimproc
+        ctrlp
+        neocomplete
+        The_NERD_Commenter
+        The_NERD_tree
+        supertab
+        tabular
+        tlib
+        vim-addon-mw-utils
+        vim-repeat
+        snipmate
+        surround
+        neco-ghc
+        vim-nix
+        fugitive
+      ];
+
+    };
+  };
+in
+  {
+    environment.systemPackages = [ myVim ];
+    environment.shellAliases.vi = "vim";
+    environment.variables.EDITOR = "vim";
+    programs.bash.shellAliases = {
+      vi = "vim";
+      svim = "sudoedit";
+    };
+    nixpkgs.config.vim.ftNix = false;
+  }
