f9298334fef4b99978fdc3c18b6d57c0b112d0ab
chessai <chessai1996@gmail.com>
2019-02-13 12:32:22 -0500

    bash aliases; extract function

--- aliases.nix
+++ aliases.nix
@@ -3,11 +3,13 @@
 {
   environment.interactiveShellInit = ''
     ### GIT 
+    alias gc='git clone'
     alias gs='git status'
     alias git-initial-commit='git commit -m "Creō ā nihilō"'
 
     ### HASKELL 
     alias drep='ghcid -c "cabal new-repl"'
+    alias nrep='nix-shell --run "ghcid -c cabal new-repl"'
 
     ### LS 
     alias ls='ls --color=auto'
@@ -15,14 +17,19 @@
     alias l.='ls -d .* --color=auto'
 
     ### CD
-    alias dev='cd ~/Development' 
+    alias dev='cd ~/development' 
     alias .0='cd .' 
     alias .1='cd ..'
     alias .2='cd ../..'
     alias .3='cd ../../..'
     alias .4='cd ../../../..'
     alias .5='cd ../../../../..'
-
+    alias .6='cd ../../../../../..'
+    alias .7='cd ../../../../../../..'
+    alias .8='cd ../../../../../../../..'
+    alias .9='cd ../../../../../../../../..'
+    alias .10='cd ../../../../../../../../..'
+    
     ### GREP
     #alias grep='grep --color=auto'
     #alias egrep='egrep --color=auto'
@@ -71,5 +78,47 @@
 
     ### SUMMONER
     alias summon='~/.local/bin/summon'
+
+    ### MISC
+    alias ax='chmod a+x'
+
+    ### extraction
+    function extract {
+     if [ -z "$1" ]; then
+        # display usage if no parameters given
+        echo "Usage: extract <path/file_name>.<zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|ex|tar.bz2|tar.gz|tar.xz>"
+     else
+        if [ -f $1 ] ; then
+            # NAME=$\{1%.*\}
+            # mkdir $NAME && cd $NAME
+            case $1 in
+              *.tar.bz2)   tar xvjf ./$1    ;;
+              *.tar.gz)    tar xvzf ./$1    ;;
+              *.tar.xz)    tar xvJf ./$1    ;;
+              *.lzma)      unlzma ./$1      ;;
+              *.bz2)       bunzip2 ./$1     ;;
+              *.rar)       unrar x -ad ./$1 ;;
+              *.gz)        gunzip ./$1      ;;
+              *.tar)       tar xvf ./$1     ;;
+              *.tbz2)      tar xvjf ./$1    ;;
+              *.tgz)       tar xvzf ./$1    ;;
+              *.zip)       unzip ./$1       ;;
+              *.Z)         uncompress ./$1  ;;
+              *.7z)        7z x ./$1        ;;
+              *.xz)        unxz ./$1        ;;
+              *.exe)       cabextract ./$1  ;;
+              *)           echo "extract: '$1' - unknown archive method" ;;
+            esac
+        else
+            echo "$1 - file does not exist"
+        fi
+     fi
+    }
   '';
+
+  # needed for 'extract' 
+  environment.systemPackages = with pkgs; [
+    p7zip
+    cabextract
+  ];
 }
