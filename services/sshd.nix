e6d2945ae3ea0b129f442b4c58758ea385954ea4
chessai <chessai1996@gmail.com>
2019-06-04 14:47:36 -0400

    configure .ssh/config with nix

--- services/sshd.nix
+++ services/sshd.nix
@@ -1,10 +1,72 @@
 {
   services.openssh = {
     enable = true;
-    
+
     permitRootLogin = "no";
 
     passwordAuthentication = false;
     challengeResponseAuthentication = false;
   };
+
+  programs.ssh.extraConfig = ''
+    Host ansible-server
+      HostName 192.168.254.254
+      User noc
+    Host collector-cos
+      HostName 10.100.112.202
+      User noc
+      ProxyJump ansible-server
+    Host qts
+      HostName 192.168.254.10
+      User noc
+      ProxyJump ansible-server
+    Host collector-iah
+      HostName 10.100.122.67
+      User noc
+      ProxyJump ansible-server
+    Host nsh-collector
+      HostName 192.168.254.10
+      User noc
+      ProxyJump ansible-server
+    Host nsh-diamond
+      HostName 167.183.11.14
+      User noc
+      ProxyJump ansible-server
+    Host lab
+      HostName 10.10.10.232
+      User amartin
+    Host lab2
+      HostName 10.10.10.98
+      User dcartwright
+    Host dpisd-collector
+      HostName 10.100.154.12
+      User noc
+      ProxyJump ansible-server
+    Host cyfair-collector
+      HostName 10.100.155.22
+      User noc
+      ProxyJump ansible-server
+    Host siem-op-1
+      HostName 192.168.254.19
+      User noc
+    Host dcg-collector
+      HostName 10.100.128.94
+      User noc
+      ProxyJump ansible-server
+    Host sch-collector
+      HostName 10.100.102.13
+      User noc
+      ProxyJump ansible-server
+    Host fcs-collector
+      HostName 10.100.100.254
+      User noc
+      ProxyJump ansible-server
+    Host kisd-collector
+      HostName 10.100.140.101
+      User noc
+      ProxyJump ansible-server
+    Host new-lab
+      HostName 10.10.10.233
+      User allsight
+  '';
 }
