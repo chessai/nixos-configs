4e9e5b95a08e795b6eee811a67532ddc840f837b
chessai <chessai1996@gmail.com>
2019-07-16 07:47:23 -0400

    add texlive overlay and make sure allow{Broken,UnsupportedSystem}=false

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,7 +1,16 @@
+{ pkgs, ... }:
+
 {
   nixpkgs = {
     config = {
       allowUnfree = true;
+      allowBroken = false;
+      allowUnsupportedSystem = false;
     };
+
+    overlays = [
+      (import ./../overlays/texlive.nix)
+    ];
   };
+
 }
