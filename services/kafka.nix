400867c74f9a6955a9ec4ce296895683f308bd80
chessai <chessai1996@gmail.com>
2019-09-26 06:56:09 -0400

    enable apache kafka; set some extra properties

--- services/kafka.nix
+++ services/kafka.nix
@@ -1,8 +1,11 @@
 {
   services.apache-kafka = {
-    enable = false;
+    enable = true;
     brokerId = 1;
     hostname = "127.0.0.1";
     zookeeper = "127.0.0.1:2181";
+    extraProperties = ''
+      offsets.topic.replication.factor=1
+    '';
   };
 }
