cf2eac99b0b6b2cd174a0a1f0253f3d76c08f0bf
chessai <chessai1996@gmail.com>
2019-09-29 13:06:29 -0400

    add a lot more systemPackages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -15,20 +15,43 @@
   # $ nix-env -qaP | grep <pkg>
   environment.systemPackages = with pkgs; [
     (if config.services.xserver.enable then gitAndTools.gitFull else git)
+    #haskell-ci
+    #json-autotype
+    #openconnect_pa
     ack
     ag # silver-searcher
+    alacritty
     bind # nslookup,dig
+    cachix
     clipmenu
+    cloc
+    cmake
     curl
+    file
+    gcc
+    ghcid
     git-lfs
+    gitAndTools.hub
+    htop
+    jq
+    mosh
+    net_snmp
+    nixops
+    #nixpkgs-pkgconfig # wrapper for pkgconfig that points it to the proper stuff
+    pavucontrol
+    sshpass
     strace
     sudo
     tcpdump
+    tcpflow
+    tldr
     tmux
+    tree
     unzip
     wget
     wpa_supplicant
     wpa_supplicant_gui
+    xfce4-14.xfce4-screenshooter #what a strange prefix.
     youtube-dl
     zip
   ];
