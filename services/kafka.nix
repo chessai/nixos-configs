12b3e14468ba70bf4c613e6d22e336aadae1fb13
chessai <chessai1996@gmail.com>
2019-09-29 17:43:02 -0400

    disable kafka and zookeeper because they make shutdown take forever

--- services/kafka.nix
+++ services/kafka.nix
@@ -1,6 +1,6 @@
 {
   services.apache-kafka = {
-    enable = true;
+    enable = false;
     brokerId = 1;
     hostname = "127.0.0.1";
     zookeeper = "127.0.0.1:2181";
--- services/zookeeper.nix
+++ services/zookeeper.nix
@@ -1,5 +1,5 @@
 {
   services.zookeeper = {
-    enable = true;
+    enable = false;
   };
 }
