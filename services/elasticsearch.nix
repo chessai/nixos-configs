d1f1038782bde8842b623fe1f0eb43aaedf4089f
chessai <chessai1996@gmail.com>
2019-10-17 10:35:33 -0400

    use elasticsearch5 (temporarily). perhaps add a flag to decide on which should be used?

--- services/elasticsearch.nix
+++ services/elasticsearch.nix
@@ -1,8 +1,31 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
+let
+  # why (import <nixpkgs> {})? because `imports` cannot
+  # depend on the value of `pkgs`. this is a rather
+  # non-obvious fact pointed out to me by clever.
+  oldNixpkgsSrc = (import <nixpkgs> {}).fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "35f0ffb7049ce7522b157807f02b9835441c8041";
+    sha256 = "0j0hrzr9b57ifwfhggpzm43zcf6wcsj8ffxv6rz7ni7ar1x99x2c";
+  };
+in
 {
+  disabledModules = [ "services/search/elasticsearch.nix" ];
+
+  imports = [
+    "${oldNixpkgsSrc}/nixos/modules/services/search/elasticsearch.nix"
+  ];
+
   services.elasticsearch = {
     enable = true;
-    package = pkgs.elasticsearch6;
+    package = pkgs.elasticsearch5;
   };
+
+  nixpkgs.overlays = [
+    (self: super: {
+      elasticsearch5 = (import oldNixpkgsSrc {}).elasticsearch5;
+    })
+  ];
 }
