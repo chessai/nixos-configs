e2e682215c707b3e41e612a71254936743d7d5d2
chessai <chessai1996@gmail.com>
2019-10-17 10:34:45 -0400

    enable and configure postgres for allsight testing

--- services/postgresql.nix
+++ services/postgresql.nix
@@ -1,5 +1,25 @@
+{ pkgs, ... }:
+
 {
   services.postgresql = {
-    enable = false;
+    enable = true;
+
+    enableTCPIP = true;
+
+    authentication = ''
+      host testdb testuser 127.0.0.1/32 md5
+    '';
+
+    ensureDatabases = [
+      "testdb"
+    ];
+
+    ensureUsers = [
+      { name = "testuser";
+        ensurePermissions = {
+          "DATABASE testdb" = "ALL PRIVILEGES";
+        };
+      }
+    ];
   };
 }
