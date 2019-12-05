f3307b3bc8a5091c0d7035c993030d89bb1c1157
chessai <chessai1996@gmail.com>
2019-12-05 17:27:02 -0500

    disable kafka

--- services/kafka.nix
+++ services/kafka.nix
@@ -1,12 +1,13 @@
 {
   services.apache-kafka = {
-    enable = true;
+    enable = false;
     brokerId = 1;
     hostname = "127.0.0.1";
     zookeeper = "127.0.0.1:2181";
     extraProperties = ''
       offsets.topic.replication.factor=1
       auto.create.topics.enable=true
+      message.timeout.ms=8000
     '';
   };
 }
