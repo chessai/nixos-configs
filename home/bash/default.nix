5567d38de5b0485b1ee7a417a904105a8e9ea1c5
chessai <chessai1996@gmail.com>
2020-06-10 01:55:15 -0700

    add some useful shell aliases. move some sessionVariables to initExtra

--- home/bash/default.nix
+++ home/bash/default.nix
@@ -25,12 +25,24 @@ with { cds = import ./cds.nix; };
 
       gist = "gist --private";
 
-      ts = "nix-shell -E 'let pkgs = import <nixpkgs> {}; in pkgs.mkShell { buildInputs = with pkgs; [ nodejs yarn nodePackages.typescript ]; }'";
+      yarn = "yarn --ignore-engines";
+      ts = "nix-shell -E 'let pkgs = import <nixpkgs> {}; in pkgs.mkShell { buildInputs = with pkgs; [ nodejs-13_x yarn nodePackages.typescript ]; }'";
 
+      sq = "sed -e 's/^\"//' -e 's/\"$//'";
+
+      # install newest nightly rust
+      rs-nightly-refresh = "nix-env -iA nixos.latest.rustChannels.nightly.rust";
     };
 
     initExtra = lib.mkBefore ''
-      . "/home/chessai/.nix-profile/etc/profile.d/hm-session-vars.sh"
+      # Only source this once.
+      if [ -n "$__HM_SESS_VARS_SOURCED" ]; then return; fi
+      export __HM_SESS_VARS_SOURCED=1
+
+      export GRAPHVIZ_DOT="${pkgs.graphviz}/bin/dot"
+      export LC_CTYPE="en_US.UTF-8";
+      export EDITOR="${pkgs.vim}/bin/vim";
+
       set -o vi
     '';
   };
