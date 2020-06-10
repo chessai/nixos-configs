c4e9fe16ddbd1a576faa5a3443799b477d5a58f1
chessai <chessai1996@gmail.com>
2020-06-10 01:57:42 -0700

    allow insecure p7zip. use mercury cache

--- nix/default.nix
+++ nix/default.nix
@@ -17,11 +17,14 @@
       "http://cache.nixos.org" # nixos.org
       "http://cache.earthtools.ca" # clever
       "https://hydra.iohk.io" # IOHK
+      "https://com-mercury-nix-cache.s3.amazonaws.com" # mercury
+      #"s3://com-mercury-nix-cache?profile=everyone"
     ];
 
     binaryCachePublicKeys = [
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" # IOHK
       "c2d.localnet-1:YTVKcy9ZO3tqPNxRqeYEYxSpUH5C8ykZ9ImUKuugf4c=" # clever
+      "com-mercury-nix-cache-1:yhfFlgvqtv0cAxzflJ0aZW3mbulx4+5EOZm6k3oML+I=" # mercury
     ];
 
     extraOptions = lib.mkOrder 1 ''
@@ -36,6 +39,9 @@
       allowUnfree = true;
       allowBroken = false;
       allowUnsupportedSystem = false;
+      permittedInsecurePackages = [
+        "p7zip-16.02"
+      ];
     };
 
     overlays = [
