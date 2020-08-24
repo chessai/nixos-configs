e1da6c5d1126950c31247e623044b792aab58747
chessai <chessai1996@gmail.com>
2020-08-24 11:45:17 -0700

    configure fonts in alacritty

--- home/alacritty/default.nix
+++ home/alacritty/default.nix
@@ -7,6 +7,26 @@
     settings = lib.mkOptionDefault {
       font = {
         size = 22.0;
+
+        normal = {
+          family = "Fira Code Nerd Font";
+          style = "Regular";
+        };
+
+        bold = {
+          family = "Fira Code Nerd Font";
+          style = "Bold";
+        };
+
+        italic = {
+          family = "Fira Code Nerd Font";
+          style = "Italic";
+        };
+
+        bold_italic = {
+          family = "Fira Code Nerd Font";
+          style = "Bold Italic";
+        };
       };
     };
   };
