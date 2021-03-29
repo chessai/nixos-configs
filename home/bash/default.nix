0b287a6a57d47dcc71c13a8c3f07ac66b0fa7050
chessai <chessai1996@gmail.com>
2021-03-29 15:52:45 -0700

    add bash alias for ghc 9. update 88 and 810 compiler alias pointers

--- home/bash/default.nix
+++ home/bash/default.nix
@@ -13,8 +13,9 @@ with { cds = import ./cds.nix; };
 
     shellAliases = cds // {
       ghc86  = "nix-shell -p haskell.compiler.ghc865";
-      ghc88  = "nix-shell -p haskell.compiler.ghc883";
-      ghc810 = "nix-shell -p haskell.compiler.ghc8101";
+      ghc884 = "nix-shell -p haskell.compiler.ghc884";
+      ghc810 = "nix-shell -p haskell.compiler.ghc8102";
+      ghc9   = "nix-shell -p haskell.compiler.ghc901";
 
       ls = "${pkgs.exa}/bin/exa -G --color auto --icons -a -s type";
       ll = "${pkgs.exa}/bin/exa -l --color always --icons -a -s type";
