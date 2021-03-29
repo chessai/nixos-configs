8136ad2266a03fb0fa466937c85e718dd2615fda
chessai <chessai1996@gmail.com>
2021-03-29 15:56:28 -0700

    correct extract implementation

--- scripts/default.nix
+++ scripts/default.nix
@@ -7,23 +7,23 @@ with rec {
     else
       if [ -f $1 ]; then
         case $1 in
-          *.tar.bz2)   ${pkgs.gnutar} xvjf ./$1    ;;
-          *.tar.gz)    ${pkgs.gnutar} xvzf ./$1    ;;
-          *.tar.xz)    ${pkgs.gnutar} xvJf ./$1    ;;
-          *.lzma)      ${pkgs.xz} --format=lzma --decompress ./$1 ;;
-          *.bz2)       ${pkgs.bzip2}/bin/bzip2 -d ./$1 ;;
-          *.rar)       ${pkgs.unrar}/bin/unrar x -ad ./$1 ;;
-          *.gz)        ${pkgs.gzip}/bin/gzip -d ./$1      ;;
-          *.tar)       ${pkgs.gnutar} xvf ./$1     ;;
-          *.tbz2)      ${pkgs.gnutar} xvjf ./$1    ;;
-          *.tgz)       ${pkgs.gnutar} xvzf ./$1    ;;
-          *.zip)       ${pkgs.unzip}/bin/unzip ./$1       ;;
-          *.Z)         ${pkgs.ncompress}/bin/uncompress ./$1  ;;
-          *.7z)        ${pkgs.p7zip}/bin/7z x ./$1        ;;
-          *.xz)        ${pkgs.xz}/bin/xz --decompress ./$1 ;;
-          *.exe)       ${pkgs.cabextract}/bin/cabextract ./$1  ;;
-          *.cab)       ${pkgs.cabextract}/bin/cabextract ./$1  ;;
-          *)           echo "extract: '$1' - unknown archive method" ;;
+          *.tar.bz2)   ${pkgs.gnutar}/bin/tar xvjf ./$1                  ;;
+          *.tar.gz)    ${pkgs.gnutar}/bin/tar xvzf ./$1                  ;;
+          *.tar.xz)    ${pkgs.gnutar}/bin/tar xvJf ./$1                  ;;
+          *.lzma)      ${pkgs.xz}/bin/xz --format=lzma --decompress ./$1 ;;
+          *.bz2)       ${pkgs.bzip2}/bin/bzip2 -d ./$1                   ;;
+          *.rar)       ${pkgs.unrar}/bin/unrar x -ad ./$1                ;;
+          *.gz)        ${pkgs.gzip}/bin/gzip -d ./$1                     ;;
+          *.tar)       ${pkgs.gnutar}/bin/tar xvf ./$1                   ;;
+          *.tbz2)      ${pkgs.gnutar}/bin/tar xvjf ./$1                  ;;
+          *.tgz)       ${pkgs.gnutar}/bin/tar xvzf ./$1                  ;;
+          *.zip)       ${pkgs.unzip}/bin/unzip ./$1                      ;;
+          *.Z)         ${pkgs.ncompress}/bin/uncompress ./$1             ;;
+          *.7z)        ${pkgs.p7zip}/bin/7z x ./$1                       ;;
+          *.xz)        ${pkgs.xz}/bin/xz --decompress ./$1               ;;
+          *.exe)       ${pkgs.cabextract}/bin/cabextract ./$1            ;;
+          *.cab)       ${pkgs.cabextract}/bin/cabextract ./$1            ;;
+          *)           echo "extract: '$1' - unknown archive method"     ;;
         esac
       else
           echo "$1 - file does not exist"
