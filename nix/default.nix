226e0f9c3ffbefd32f99bba77797069dc4cf3813
chessai <chessai1996@gmail.com>
2021-12-27 12:28:54 -0800

    update iohk caches

--- nix/default.nix
+++ nix/default.nix
@@ -26,11 +26,11 @@ in
     binaryCaches = [
       "http://cache.nixos.org" # nixos.org
       # "http://cache.earthtools.ca" # clever
-      "https://hydra.iohk.io" # IOHK
+      "https://hydra.iohk.io" "https://iohk.cachix.org" # IOHK
     ];
 
     binaryCachePublicKeys = [
-      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" # IOHK
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo=" # IOHK
       # "c2d.localnet-1:YTVKcy9ZO3tqPNxRqeYEYxSpUH5C8ykZ9ImUKuugf4c=" # clever
     ];
 
