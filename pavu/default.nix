ff6434ab9fa047ec39a1f1f47b80c0b3be9bf1bc
chessai <chessai1996@gmail.com>
2022-01-28 00:06:44 -0800

    beef up pavu

--- pavu/default.nix
+++ pavu/default.nix
@@ -1,15 +1,23 @@
+# Explanation of bluetooth configuration can be found here: https://nixos.wiki/wiki/Bluetooth
 { config, pkgs, ... }:
 
 {
+  home-manager.users.chessai.services.mpris-proxy.enable = true;
+
   services.blueman.enable = true;
 
   hardware = {
-    bluetooth.enable = true;
+    bluetooth = {
+      enable = true;
+
+      package = pkgs.bluezFull;
+    };
 
     pulseaudio = {
-      # enable pulseaudio volume control.
-      # this can configured via pkgs.pavucontrol.
+      # pulseaudio can configure system audio via `pkgs.pavucontrol`.
       enable = true;
+
+      package = pkgs.pulseaudioFull;
     };
   };
 }
